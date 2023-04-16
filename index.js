171e947071ed474bfe12f0f7532e415ac0bb8f8c
Jash <jashhinger62@gamil.com>
2023-04-16 17:15:20 +0530

    added some bootstrap

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ io.on('connection', (socket) => {
 
   socket.on('msg_send', (data) => {
     console.log(data)
-    // io.emit('msg_rcvd', data)  // using io all client , all client react
+    io.emit('msg_rcvd', data)  // using io all client , all client react
     // socket.emit('msg_rcvd', data)  // now other browser will not react, it is for same client\
-    socket.broadcast.emit('msg_rcvd', data) // except the original one , other client will recieve message
+    // socket.broadcast.emit('msg_rcvd', data) // except the original one , other client will recieve message
   })
 })
 
